6b45053756d71b0ac19586794c928c7218d6e52b
junya2632 <21rd038@ms.dendai.ac.jp>
2024-08-31 23:42:35 +0900

    コントローラーとルーティングの設定が完了しました

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Post;
 
 class PostController extends Controller
 {
-    public function index(Post $post)
+    // 投稿の一覧を表示
+    public function index()
     {
-        return $post->get();
+        return view('posts.index'); // 投稿一覧のビューを表示
+    }
+
+    // 新しい投稿の作成フォームを表示
+    public function create()
+    {
+        return view('posts.create'); // 投稿作成フォームのビューを表示
+    }
+
+    // 新しい投稿をデータベースに保存
+    public function store(Request $request)
+    {
+        // バリデーションと投稿の保存処理をここに記述
+    }
+
+    // 特定の投稿の詳細を表示
+    public function show($id)
+    {
+        return view('posts.show', compact('id')); // 投稿の詳細ビューを表示
+    }
+
+    // 投稿の編集フォームを表示
+    public function edit($id)
+    {
+        return view('posts.edit', compact('id')); // 投稿編集フォームのビューを表示
+    }
+
+    // 編集された投稿をデータベースに保存
+    public function update(Request $request, $id)
+    {
+        // バリデーションと投稿の更新処理をここに記述
+    }
+
+    // 投稿を削除
+    public function destroy($id)
+    {
+        // 投稿の削除処理をここに記述
     }
 }
